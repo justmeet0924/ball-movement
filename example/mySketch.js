a9325909e60eb9957051870282383cece865a027
justmeet0924 <93417919+justmeet0924@users.noreply.github.com>
2023-12-12 14:31:02 +0900

    Update mySketch.js

--- example/mySketch.js
+++ example/mySketch.js
@@ -1,1 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
 
+  bs = [];
+
+
+  for (let i = 0; i < 5; i++) {
+    bs.push(new Ball(100, 20,"black"));
+  }
+	
+  bs.forEach((b) => b.init(bs));
+
+
+}
